ea3672dc7426a01e7cc3c520a1f0fe33d814a267
weargle <569018447@qq.com>
2024-05-03 23:49:54 +0800

    测试

--- src/pages/api/firecrawl/crawlUtil.ts
+++ src/pages/api/firecrawl/crawlUtil.ts
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://cksp.asia:45127/v0/scrape';
+const BASE_URL = process.env.BASE_URL || 'http://cksp.asia:45127/v0/scrape';
+const VERCEL_ACCESS_TOKEN = process.env.VERCEL_ACCESS_TOKEN || 'BpP9zOerpJuHRLAfQKONJ5p1'
 
 const crawlResult = async (param: string) => {
 
@@ -6,6 +7,7 @@ const crawlResult = async (param: string) => {
         body: param,
         headers: {
             'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + VERCEL_ACCESS_TOKEN
         },
         method: 'POST'
     });
