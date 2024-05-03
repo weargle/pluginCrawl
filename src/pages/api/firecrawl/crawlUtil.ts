f8a2764a90cae248e00a0f4ef9c39b8093233268
weargle <569018447@qq.com>
2024-05-04 01:54:28 +0800

    添加验证测试

--- src/pages/api/firecrawl/crawlUtil.ts
+++ src/pages/api/firecrawl/crawlUtil.ts
@@ -1,12 +1,12 @@
-const BASE_URL = process.env.BASE_URL || 'http://cksp.asia:45127/v0/scrape';
-const VERCEL_ACCESS_TOKEN = process.env.VERCEL_ACCESS_TOKEN || 'BpP9zOerpJuHRLAfQKONJ5p1'
+const BASE_URL = process.env.BASE_URL || 'http://cksp.asia/v0/scrape';
+//const VERCEL_ACCESS_TOKEN = process.env.VERCEL_ACCESS_TOKEN || 'BpP9zOerpJuHRLAfQKONJ5p1'
 
 const crawlResult = async (param: string) => {
 
     const result = await fetch(BASE_URL, {
         body: param,
         headers: {
-            'Authorization': 'Bearer ' + VERCEL_ACCESS_TOKEN,
+            'Authorization': 'Bearer ${process.env.VERCEL_ACCESS_TOKEN}',
             'Content-Type': 'application/json'
         },
         method: 'POST'
